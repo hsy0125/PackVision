9da751f5c580531d13dea07a1fc20b8790a6e0c8
user <ocsy0125@naver.com>
2026-03-30 11:22:42 +0900

    init project

---
+++ PackVisionApp/Models/imsi2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackVisionApp.Models
+{
+	internal class imsi2
+	{
+	}
+}
---
+++ PackVisionApp/Utils/imsi3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackVisionApp.Utils
+{
+	internal class imsi3
+	{
+	}
+}
